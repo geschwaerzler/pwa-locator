2848c71923c0ce13e0686700ad49d3c8f78fbe18
Gerhard Schwaerzler <gerhard.schwaerzler@nu-gmbh.com>
2023-11-12 19:24:42 +0100

    workbox recipes for page, resource and image caches.

--- src/serviceworker.js
+++ src/serviceworker.js
@@ -1,34 +1,13 @@
 import { manifest } from '@parcel/service-worker';
 import { CacheableResponsePlugin } from 'workbox-cacheable-response';
 import { ExpirationPlugin } from 'workbox-expiration';
-import { warmStrategyCache } from 'workbox-recipes';
+import { imageCache, pageCache, staticResourceCache } from 'workbox-recipes';
 import { registerRoute } from 'workbox-routing';
-import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
+import { CacheFirst } from 'workbox-strategies';
+
+console.debug(`serviceworker.js got manifest: ${manifest}`);
 
 self.__WB_DISABLE_DEV_LOGS = true;
-
-// Set up page cache
-const assetsCache = new StaleWhileRevalidate({
-    cacheName: 'asset-cache',
-    plugins: [
-        new CacheableResponsePlugin({
-            statuses: [0, 200],
-        })
-    ],
-});
-
-const imageCache = new CacheFirst({
-    cacheName: 'image-cache',
-    plugins: [
-        new CacheableResponsePlugin({
-            statuses: [0, 200],
-        }),
-        new ExpirationPlugin({
-            maxAgeSeconds: 24 * 60 * 60,
-            maxEntries: 100,
-        }),
-    ],
-});
 
 const tileCache = new CacheFirst({
     cacheName: 'tile-cache',
@@ -43,20 +22,19 @@ const tileCache = new CacheFirst({
     ],
 });
 
-console.debug(`serviceworker.js got manifest: ${manifest}`);
-
-warmStrategyCache({
-    urls: manifest.filter(urlPath => /\.(html|css|js)/.test(urlPath)),
-    strategy: assetsCache,
-});
-
-warmStrategyCache({
-    urls: manifest.filter(urlPath => /\.(png|svg)/.test(urlPath)),
-    strategy: imageCache,
-});
-
 registerRoute(
-    ({ url }) => {
-        console.debug(`registerRoute for url.hostname: ${url.hostname}`);
-        return url.hostname === 'tile.openstreetmap.org';
-    }, tileCache);
+    ({ url }) => url.hostname === 'tile.openstreetmap.org',
+    tileCache
+);
+
+// Set up caches using workbox recipes
+pageCache({
+    warmCache: manifest.filter(urlPath => /\.(html)/.test(urlPath))
+});
+staticResourceCache({
+    warmCache: manifest.filter(urlPath => /\.(css|js)/.test(urlPath))
+});
+imageCache({
+    warmCache: manifest.filter(urlPath =>
+        !urlPath.startsWith('/locator') && /\.(png|svg)/.test(urlPath))
+});
